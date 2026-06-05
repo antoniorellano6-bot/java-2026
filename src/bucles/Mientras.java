6dc6fb709f5969b6fb6daa885493883db7bda2de
antoni <antoniorellano6@gmail.com>
2026-06-05 14:06:33 -0500

    subiendo mi trabajo de bucles

---
+++ src/bucles/Mientras.java
@@ -0,0 +1,20 @@
+package bucles;
+
+public class Mientras {
+    public static void main(String[] args) {
+     int contador = 0;
+
+        while (contador <= 100){
+            System.out.println(contador);
+            contador=contador+2;
+
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ src/bucles/TablaMultiplicar.java
@@ -0,0 +1,16 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+       int contador=1;
+       while (contador<=10){
+           System.out.println("5 x" + contador + "=" + (5 * contador));
+           contador++;
+       }
+
+
+
+    }
+}
